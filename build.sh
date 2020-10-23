5c094bc8facf77113c562e69d0aaa55f02f7a5b6
apenugon <akulvp@gmail.com>
2020-10-23 10:53:11 -0400

    Add spark-network creation to build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ docker build -t akul/spark3rapids:base .
 docker build -t akul/spark3rapids:jupyter jupyter
 docker build -t akul/spark3rapids:master -f spark_master/Dockerfile spark_master 
 docker build -t akul/spark3rapids:worker -f spark_worker/Dockerfile spark_worker
+docker network create spark-network
